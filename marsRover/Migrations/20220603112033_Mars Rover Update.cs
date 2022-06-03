5e0888a4268a316018dea832ca3e4753c4b9e8d7
SRNikolov18 <SRNikolov18@codingburgas.bg>
2022-06-03 14:22:47 +0300

    final changes

---
+++ marsRover/Migrations/20220603112033_Mars Rover Update.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace marsRover.Migrations
+{
+    public partial class MarsRoverUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- marsRover/Startup.cs
+++ marsRover/Startup.cs
@@ -31,7 +31,7 @@ namespace marsRover
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "marsRover", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "mars Rover", Version = "v1" });
             });
         }
 
